6af922c047805dd637fb3ad213bb509552cabd46
caoxiaolin <caoxiaolin@feiyi.com.cn>
2018-03-14 17:07:04 +0800

    添加 fastJson

---
+++ src/main/java/com/dream/config/WebConfig.java
@@ -0,0 +1,24 @@
+package com.dream.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * Description:
+ * User: caoxiaolin
+ * Date: 2018-03-14
+ * Time: 15:59
+ */
+@Configuration
+public class WebConfig {
+
+    public WebMvcConfigurer corsConfigurer(){
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**");
+            }
+        };
+    }
+}
